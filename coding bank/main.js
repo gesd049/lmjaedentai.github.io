0cf63817579adc088d7c3bdb183ea6122b829a74
lmjaedentai <63090071+lmjaedentai@users.noreply.github.com>
2021-06-28 12:23:11 +0800

    beta v2
    better user interface,
    hide title when search,
    add share & like button

--- coding bank/main.js
+++ coding bank/main.js
@@ -1,19 +1,37 @@
-
+// main
 function search() {
-  let input = document.getElementById('searchbar').value
-  input=input.toLowerCase();
-  let x = document.getElementsByTagName('h2');
+  let searchstring = document.getElementById('searchbar').value;
+  searchstring = searchstring.toLowerCase();
+  let searchvalue = document.getElementsByTagName('h2');
+
+  document.getElementById('titlebox').style.display = 'none';
     
-  for (i = 0; i < x.length; i++) { 
-    if (!x[i].innerHTML.toLowerCase().includes(input)) {
-      x[i].parentElement.style.display="none";
+  for (i = 0; i < searchvalue.length; i++) { 
+    if (!searchvalue[i].innerHTML.toLowerCase().includes(searchstring)) {
+      searchvalue[i].parentElement.style.display = "none";
     }
     else {
-      x[i].parentElement.style.display="block";                 
+      document.getElementById('searchresultpadd').style.padding = '20px'
+      searchvalue[i].parentElement.style.display = "block"; 
+      document.getElementById('home').style.display = 'block';
     }
+  }  
+  if(searchstring == ''){
+    document.getElementById('titlebox').style.display = 'block';
+    document.getElementById('searchresultpadd').style.padding = '0px'
+    document.getElementById('home').style.display = 'block';
   }
 }
 
+
+
+
+
+// style
 function liked(){
   document.getElementById('heart').style.color = "red";
+}
+
+function shared(){
+  document.getElementById('share').style.color = "rgb(0, 255, 115)";
 }
