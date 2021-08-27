7bc3e4885931d96c7cc4c3f7412434cf14565e56
Jaeden Tai <taijaeden@gmail.com>
2021-08-27 16:01:14 +0800

    update popspike

---
+++ popspike/main.js
@@ -0,0 +1,22 @@
+var img = document.getElementById("spike");
+var audio = new Audio('pop.mp3');
+
+// mouseclick event
+img.addEventListener("mousedown", function(){
+    img.src = '2.1.png';
+    audio.play();
+});
+    
+img.addEventListener("mouseup", function(){
+    img.src = '1.1.png';
+});
+
+// touch event
+img.addEventListener("touchstart", function(){
+    img.src = '2.png';
+    audio.play();
+});
+
+img.addEventListener("touchmove", function(){
+    img.src = '1.png';
+});
