c3b17cdba8a8eaedf54e9cb94458f30098172a92
Jaeden Tai <taijaeden@gmail.com>
2021-08-27 19:40:49 +0800

    improve loading time (test)

--- popspike/main.js
+++ popspike/main.js
@@ -1,16 +1,14 @@
 var img = document.getElementById("spike");
 var audio = new Audio('pop.mp3');
-
 // mouseclick event
 img.addEventListener("mousedown", function(){
-    img.src = '2.1.png';
+    img.src = '2.png';
     audio.play();
 });
-    
-img.addEventListener("mouseup", function(){
-    img.src = '1.1.png';
-});
 
+img.addEventListener("mouseup", function(){
+    img.src = '1.png';
+});
 // touch event
 img.addEventListener("touchstart", function(){
     img.src = '2.png';
