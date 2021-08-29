5e51be487729f638c7ca0592ce9de1c518750ceb
Jaeden Tai <taijaeden@gmail.com>
2021-08-29 12:00:56 +0800

    new stuff

--- popspike/main.js
+++ popspike/main.js
@@ -1,20 +1,46 @@
-var img = document.getElementById("spike");
-var audio = new Audio('pop.mp3');
+var spike = document.getElementById("spike");
+var audio = new Audio('./asset/pop.mp3');
+var scoreboard = document.getElementById('scoreboard');
+
+if(Cookies.get('score') == 'NaN'){
+    var score = 0;
+    Cookies.set('score',0);
+}
+else{
+    var score = Cookies.get('score');
+    scoreboard.innerHTML = score;
+}
+
+function removecookie(){
+    Cookies.set('score',0);
+    console.log('removed');
+    location.reload();
+}
+
 // mouseclick event
-img.addEventListener("mousedown", function(){
-    img.src = '2.png';
+spike.addEventListener("mousedown", function(){
+    spike.src = './asset/2.png';
     audio.play();
 });
 
-img.addEventListener("mouseup", function(){
-    img.src = '1.png';
+spike.addEventListener("mouseup", function(){
+    spike.src = './asset/1.png';
+    increaseScore();
 });
+
 // touch event
-img.addEventListener("touchstart", function(){
-    img.src = '2.png';
+spike.addEventListener("touchstart", function(){
+    spike.src = './asset/2.png';
     audio.play();
 });
 
-img.addEventListener("touchmove", function(){
-    img.src = '1.png';
+spike.addEventListener("touchmove", function(){
+    spike.src = './asset/1.png';
+    increaseScore();
 });
+
+function increaseScore(){
+    score++;
+    scoreboard.innerHTML = score;
+    Cookies.set('score', score);
+}
