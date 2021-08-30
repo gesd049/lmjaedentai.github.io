6ded7650504b51e41d1b61001f11e06e91f53ff0
Jaeden Tai <taijaeden@gmail.com>
2021-08-30 09:22:24 +0800

    fixed cookie undefined issue

--- popspike/main.js
+++ popspike/main.js
@@ -2,38 +2,27 @@ var spike = document.getElementById("spike");
 var audio = new Audio('./asset/pop.mp3');
 var scoreboard = document.getElementById('scoreboard');
 
-if(Cookies.get('score') == 'NaN'){
-    var score = 0;
-    Cookies.set('score',0);
-}
-else{
-    var score = Cookies.get('score');
-    scoreboard.innerHTML = score;
-}
+var score = Cookies.get('score');
+scoreboard.innerHTML = score;
 
-function removecookie(){
+if(scoreboard.innerHTML == 'NaN' || scoreboard.innerHTML == 'undefined'){ //if no cookie
     Cookies.set('score',0);
-    console.log('removed');
     location.reload();
 }
 
-// mouseclick event
+// mouseclick & touchscreen event
 spike.addEventListener("mousedown", function(){
     spike.src = './asset/2.png';
     audio.play();
 });
-
 spike.addEventListener("mouseup", function(){
     spike.src = './asset/1.png';
     increaseScore();
 });
-
-// touch event
 spike.addEventListener("touchstart", function(){
     spike.src = './asset/2.png';
     audio.play();
 });
-
 spike.addEventListener("touchmove", function(){
     spike.src = './asset/1.png';
     increaseScore();
